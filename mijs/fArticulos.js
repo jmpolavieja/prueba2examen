11dfc5dd9187a3504211c5c013c512d627e3453f
Josemaría García de Polavieja Ferre <jgf@altair.edu.es>
2024-10-14 14:39:51 +0200

    primero

---
+++ mijs/fArticulos.js
@@ -0,0 +1,13 @@
+const listaArticulos = [
+    'raton', 'teclado', 'pantalla', 'alfombrilla', 'torreCPU', 'procesador', 'tarjetaGrafica'
+];
+
+export function listaHTML() {
+    // Creamos una lista de artículos
+    const ul = document.createElement('ul');
+    // Por cada artículo un li
+    listaArticulos.map( articulo => {
+        const li = document.createElement('li');
+        const text = document.createTextNode(articulo);
+    } )
+}
